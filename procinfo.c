3570f219e94b40338f5c6b028edd75d263171696
C-A de Salaberry <cadesalaberry@yahoo.com>
2013-01-16 16:43:33 -0500

    Added: file reader in mass1

--- procinfo.c
+++ procinfo.c
@@ -3,7 +3,7 @@
 
 // Needed to get PID of a process.
 #include <unistd.h>
-
+#include <string.h>
 // Needed for I/O to stdout.
 #include <stdio.h>
 
@@ -29,36 +29,63 @@
 
 // Returns accordingly: 0=SUCESS, 1=ERROR
 //int unsetenv(const char *name);
+int addValueToFieldInFile(char * field, FILE * file, char * buffer);
+extern char ** environ;
 
-int main(){
-	
-	printf("\n\n========================================\n");
-	printf("Running process #%d", getpid());
-	printf(" with parent #%d\n", getppid());
+int main() {
 
-	FILE * pFile;
-	char * pName;
-	char * pPath;
-	
-	// sprintf(); is the way to go for concatenation.
-	//fprintf(pPath, "/proc/%d", getpid());
+	  // Gets the program signature.
+	  pid_t pid = getpid();
+	  pid_t ppid = getppid();
 
-	pFile = fopen (pPath,"r");
+	  FILE * file;
+	  char * pPath;
+	  char report[6969];
+	  char * field;
+	  char * buffer;
+	  
+	  // Gets the path the the status file.
+	  sprintf(pPath, "/proc/%d/status", pid);
 
-	if (pFile != NULL) {
-		fscanf(pFile, "Name:\t%[^\n]\n", *pName);
-		printf("%s", &pName);		
-		fclose (pFile);
-	} else {	
-		printf("Failed while opening: /proc\n");	
-	}
-	
-	printf("\n========================================\n\n");
-	
+	  file = fopen(pPath, "r");
+	  if (file != NULL) {
+			printf("Reading: %s\n", pPath);
+
+			//strcat(report, addValueToFieldInFile("Name:", file));
+
+			addValueToFieldInFile("Name:", file, buffer);
+			printf("%s", buffer);
+			// Get the values in there
+			fclose(file);
+	  } else {
+			printf("Failed while opening: %s\n", pPath);
+	  }
+
+	  //  is the way to go for concatenation.
+	  //fprintf(pPath, "/proc/%d", getpid());
+
+	  printf("\n\n========================================\n");
+	  printf("Running process #%d", pid);
+	  printf(" with parent #%d\n", ppid);
+	  printf("%s", report);
+	  printf("\n========================================\n\n");
+
 }
 
+int addValueToFieldInFile(char * field, FILE * file, char * buffer) {
 
-
-
-
-
+	  buffer = malloc(sizeof (char)* 6969);
+	   while (!feof(file)) {
+			
+			fscanf(file, "%s", buffer);
+			
+			printf("Looking at: %s\n", buffer);
+			//exit(1);
+			if (strcmp(buffer, field) == 0) {
+				  strcat(field, " ");
+				  fscanf(file, "%s", buffer);
+				  return 1;
+			}
+	  }
+	  return -1;
+}
